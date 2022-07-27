d22f4f3056681650b9de78d39424f7311902d302
alexis00rod <alexis00rodrigo@gmail.com>
2022-07-26 23:14:30 -0300

    Configure rutas principales

--- src/index.js
+++ src/index.js
@@ -1,17 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { BrowserRouter,Routes,Route } from 'react-router-dom';
+import { App } from './App'
+import { MyNotes } from './routes/MyNotes/MyNotes';
+import { Account } from './routes/Account/Account';
+import { Settings } from './routes/Settings/Settings';
+import { Trash } from './routes/Trash/Trash';
+import './index.scss'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <BrowserRouter>
+        <Routes>
+            <Route path='/' element={<App />}>
+                <Route index element={<MyNotes />} />
+                <Route path='account' element={<Account />}/>
+                <Route path='settings' element={<Settings />} />
+                <Route path='trash' element={<Trash />} />
+            </Route>
+        </Routes>
+        </BrowserRouter>
+    </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
